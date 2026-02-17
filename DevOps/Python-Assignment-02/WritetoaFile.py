7f2aa8a90370534ff6a0d35526d1477879a9868a
praveen <gbpraveen55@gmail.com>
2026-02-17 07:42:15 +0530

    Python Assignment

---
+++ DevOps/Python-Assignment-02/WritetoaFile.py
@@ -0,0 +1,6 @@
+file = open("sample.txt", "w")
+file.write("Hello, this is a sample text file.\n")
+file.write("This content is written using Python.\n")
+file.close()
+
+print("Content written to file successfully.")
