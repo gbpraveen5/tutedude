34ddd69f9d938dc3d263f75af7a6bcb91abcef39
praveen <gbpraveen55@gmail.com>
2026-02-28 17:26:57 +0530

    docker assigment

---
+++ DevOps/05-Docker Frontend Assignment/fullstack-app/backend/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.json
+    name = data.get("name")
+    email = data.get("email")
+
+    response = {
+        "message": "Data received successfully",
+        "name": name,
+        "email": email
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
